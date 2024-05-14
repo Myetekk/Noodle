95d8ba351cb202f717cab2923b1e4e3824c4a950
Myetekk <mirekmich19@gmail.com>
2024-05-14 12:09:45 +0200

    jednak przeniesienie części z powrotem
    XD

--- src/Pages/Logging/LogIn.js
+++ src/Pages/Logging/LogIn.js
@@ -4,7 +4,7 @@ import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 import './Logging.css';
 import logo from '../../logo.svg';
-import { courseInfo } from '../MainPage/MainPage';
+import { getUsersCoursesInfo } from '../MainPage/MainPage';
 
 
 
@@ -140,40 +140,6 @@ function LogIn() {
         });
 
         await getUsersCoursesInfo(navigate, userCourseId)
-    }
-
-
-
-    async function getUsersCoursesInfo(navigate, userCourseId) {
-
-        console.log(userCourseId)
-        await axios.post('http://localhost:3001/api/loadcourses', userCourseId)
-        .then( response => {
-            const userCoursesTemp = response.data;
-            
-            userCoursesTemp.forEach( (element) => {
-                userCourses.push(
-                    <div className="Courses" onClick={() => navigateToCourse(navigate, element.course_id, element.course_name, element.course_owner)}>
-                        <text className='Courses-title'>{element.course_name}</text>
-                        <text className='Courses-description'>prowadzący kursu: {element.course_owner}</text>
-                    </div>
-                )
-            })
-
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-    }
-
-
-
-    // przekazuje informacje o wybranym (klikniętym) kursie i przekierowywuje do niego
-    function navigateToCourse(navigate, current_course_id, current_course_name, current_course_owner) {
-        courseInfo.course_id = current_course_id
-        courseInfo.course_name = current_course_name
-        courseInfo.course_owner = current_course_owner
-        navigate("/course")
     }
 
 
--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -65,4 +65,34 @@ export default MainPage;
 
 
 
+export async function getUsersCoursesInfo(navigate, userCourseId) {
 
+    console.log(userCourseId)
+    await axios.post('http://localhost:3001/api/loadcourses', userCourseId)
+    .then( response => {
+        const userCoursesTemp = response.data;
+        
+        userCoursesTemp.forEach( (element) => {
+            userCourses.push(
+                <div className="Courses" onClick={() => navigateToCourse(navigate, element.course_id, element.course_name, element.course_owner)}>
+                    <text className='Courses-title'>{element.course_name}</text>
+                    <text className='Courses-description'>prowadzący kursu: {element.course_owner}</text>
+                </div>
+            )
+        })
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
+
+
+
+// przekazuje informacje o wybranym (klikniętym) kursie i przekierowywuje do niego
+function navigateToCourse(navigate, current_course_id, current_course_name, current_course_owner) {
+    courseInfo.course_id = current_course_id
+    courseInfo.course_name = current_course_name
+    courseInfo.course_owner = current_course_owner
+    navigate("/course")
+}
