17ba36a19eeb56ea7a7ac77d334f0ddb80b909e2
Myetekk <mirekmich19@gmail.com>
2024-05-14 13:18:38 +0200

    wypisywanie elementów w kursach
    pobiera elementy z bazy i pokazuje je w kursach do których są przypisane

--- src/Pages/CoursePage/CoursePage.js
+++ src/Pages/CoursePage/CoursePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import axios from 'axios';
 
 import './CoursePage.css';
 import '../../App.css';
 import TopBar from '../../Assets/TopBar/TopBar';
-import { courseInfo } from '../MainPage/MainPage';
+import { courseInfo, coursesElements } from '../MainPage/MainPage';
 
 
 
@@ -35,11 +36,13 @@ function CoursePage() {
 
         <div className='Course-container'>
           
-          <div className='Course-segment'>
+          {/* <div className='Course-segment'>
             <text className='Course-title'>id: {courseInfo.course_id}</text>
             <text>name: {courseInfo.course_name}</text>
             <text>owner: {courseInfo.course_owner}</text>
-          </div>
+          </div> */}
+
+          { coursesElements }
 
         </div>
 
@@ -53,3 +56,34 @@ function CoursePage() {
 }
 
 export default CoursePage
+
+
+
+
+
+
+
+
+
+
+export async function getCoursesElementsInfo(navigate, elementId) {
+    await axios.post('http://localhost:3001/api/loadelements', elementId)
+    .then( response => {
+        const coursesElementsTemp = response.data;
+        
+        coursesElementsTemp.forEach( (element) => {
+          coursesElements.push(
+                <div className="Courses">
+                    <text className='Courses-title'>{element.name}</text>
+                    <text className='Courses-description'>opis: {element.description}</text>
+                    <text className='Courses-description'>otwarcie: {element.open_date}</text>
+                    <text className='Courses-description'>zamknięcie: {element.close_date}</text>
+                </div>
+            )
+        })
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -6,12 +6,14 @@ import './MainPage.css';
 import '../../App.css';
 import TopBar from '../../Assets/TopBar/TopBar';
 import { userInfo, userCourses } from '../Logging/LogIn';
+import { getCoursesElementsInfo } from '../CoursePage/CoursePage';
 
 
 
 
 
 export let courseInfo = {course_id: 0, course_name: "", course_owner: ""} 
+export let coursesElements = []
 
 
 
@@ -43,7 +45,7 @@ function MainPage() {
 
                 <div className='Courses-container'>
                     
-                    {userCourses}
+                    { userCourses }
 
                 </div>
 
@@ -65,9 +67,22 @@ export default MainPage;
 
 
 
-export async function getUsersCoursesInfo(navigate, userCourseId) {
+// zeruje informacje aby uniknąć wyświetlania kilka razy to samo po przejściu 'poprzednia strona' 'następna strona'
+function clearInformation() {
+    courseInfo = {course_id: 0, course_name: "", course_owner: ""} 
+    coursesElements = []
+}
 
-    console.log(userCourseId)
+
+
+
+
+
+
+
+
+
+export async function getUsersCoursesInfo(navigate, userCourseId) {
     await axios.post('http://localhost:3001/api/loadcourses', userCourseId)
     .then( response => {
         const userCoursesTemp = response.data;
@@ -90,9 +105,41 @@ export async function getUsersCoursesInfo(navigate, userCourseId) {
 
 
 // przekazuje informacje o wybranym (klikniętym) kursie i przekierowywuje do niego
-function navigateToCourse(navigate, current_course_id, current_course_name, current_course_owner) {
+async function navigateToCourse(navigate, current_course_id, current_course_name, current_course_owner) {
+
+    clearInformation()
+    
     courseInfo.course_id = current_course_id
     courseInfo.course_name = current_course_name
     courseInfo.course_owner = current_course_owner
+
+    await getCoursesElements(navigate)
+
     navigate("/course")
+}
+
+
+
+// pobiera informacje o elementach kursu
+async function getCoursesElements(navigate)  {
+    const course_id = { course_id_: courseInfo.course_id }
+    const courseElementId = [];
+    let courseElementIdTemp;
+
+    await axios.post('http://localhost:3001/api/courseelements', course_id)
+    .then( response => {
+        courseElementIdTemp = response.data;
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
+    courseElementIdTemp.forEach( (element) => {
+        courseElementId.push( element.element_id )
+    });
+
+    console.log(courseElementId)
+
+    // await getCoursesElementsInfo(navigate, [5, 6, 7, 8, 9, 10])
+    await getCoursesElementsInfo(navigate, courseElementId)
 }
